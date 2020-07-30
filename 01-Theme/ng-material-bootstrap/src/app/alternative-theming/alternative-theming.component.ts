eb0fb1babeebb98e659315baa09a289f623d83d1
Alexander Pajer <alexander.pajer@integrations.at>
2020-07-30 08:31:57 +0200

    Theming custom components

---
+++ 01-Theme/ng-material-bootstrap/src/app/alternative-theming/alternative-theming.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alternative-theming',
+  templateUrl: './alternative-theming.component.html',
+  styleUrls: ['./alternative-theming.component.scss']
+})
+export class AlternativeThemingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- 01-Theme/ng-material-bootstrap/src/app/app.module.ts
+++ 01-Theme/ng-material-bootstrap/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClassicThemingComponent } from './classic-theming/classic-theming.component';
+import { AlternativeThemingComponent } from './alternative-theming/alternative-theming.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassicThemingComponent,
+    AlternativeThemingComponent
   ],
   imports: [
     BrowserModule,
