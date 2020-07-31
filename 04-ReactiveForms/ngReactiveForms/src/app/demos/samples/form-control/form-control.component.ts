573cc328f4f8e58e9e7e9fa4050c5039d9a41965
Alexander Pajer <alexander.pajer@integrations.at>
2020-07-31 12:06:20 +0200

    FormControl

--- 04-ReactiveForms/ngReactiveForms/src/app/demos/samples/form-control/form-control.component.ts
+++ 04-ReactiveForms/ngReactiveForms/src/app/demos/samples/form-control/form-control.component.ts
@@ -9,9 +9,27 @@ import { FormControl, Validators } from '@angular/forms';
 export class FormControlComponent implements OnInit {
   constructor() {}
 
-  name = new FormControl('Giro');
+  name = new FormControl('Giro', [Validators.required]);
   postal = new FormControl('3544');
   city = new FormControl('Idolsberg', [Validators.maxLength(15)]);
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscribeNameChanges();
+  }
+
+  subscribeNameChanges() {
+    this.name.valueChanges.subscribe((data) =>
+      console.log('Form values changed', data)
+    );
+    this.name.statusChanges.subscribe((data) =>
+      console.log('Form status changed', data)
+    );
+    if (this.name.errors) {
+      this.name.errors.subscribe((data) => console.log('Form errors:', data));
+    }
+  }
+
+  updateName() {
+    this.name.setValue('Soi - Gladdenfields Beeing Verry Pretty');
+  }
 }
