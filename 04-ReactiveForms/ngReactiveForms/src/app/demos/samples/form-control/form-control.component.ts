529a6f04d106a677efbb216479b32b6c434261f8
Alexander Pajer <alexander.pajer@integrations.at>
2020-07-31 11:58:20 +0200

    formcontrol

--- 04-ReactiveForms/ngReactiveForms/src/app/demos/samples/form-control/form-control.component.ts
+++ 04-ReactiveForms/ngReactiveForms/src/app/demos/samples/form-control/form-control.component.ts
@@ -9,6 +9,7 @@ import { FormControl, Validators } from '@angular/forms';
 export class FormControlComponent implements OnInit {
   constructor() {}
 
+  name = new FormControl('Giro');
   postal = new FormControl('3544');
   city = new FormControl('Idolsberg', [Validators.maxLength(15)]);
 
