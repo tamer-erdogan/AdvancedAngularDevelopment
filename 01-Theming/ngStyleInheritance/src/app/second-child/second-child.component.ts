3083ea091ebec0dfa50995e0556a13b3b6810968
Alexander Pajer <alexander.pajer@integrations.at>
2020-08-05 10:00:13 +0200

    base

--- 01-Theming/ngStyleInheritance/src/app/second-child/second-child.component.ts
+++ 01-Theming/ngStyleInheritance/src/app/second-child/second-child.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-second-child',
   template: ` <div class="divclass">Second Child</div> `,
-  styles: [``],
+  styles: [
+    `
+      .divclass {
+        border: green 2px solid;
+      }
+    `,
+  ],
 })
 export class SecondChildComponent implements OnInit {
   constructor() {}
